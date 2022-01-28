d80bec5c49c5a18d2ff5cf61f8891903fbc48713
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-01-28 23:34:26 +0100

    reorder properties to match source code

--- blog/admin.py
+++ blog/admin.py
@@ -7,11 +7,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'contents']
-    summer_note = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
 
 
 @admin.register(Comment)
